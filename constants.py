37430b5d726aae51ca8e207bc7da9b6c7b15d750
BareBeaverBat <salisbury.100@buckeyemail.osu.edu>
2024-11-05 15:39:00 -0500

    prototype of project structure for data gen stage

---
+++ constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+schemas_path = Path("json_schemas")
+objects_path = Path("json_objects")
+texts_path = Path("text_passages")
+
+google_api_key_env = "GOOGLE_DEEPMIND_API_KEY"
+anthropic_api_key_env = "ANTHROPIC_API_KEY"
